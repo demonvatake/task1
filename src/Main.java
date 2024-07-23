919f87ba33035fadfd625fd7bdcde4de6303dfc2
Demonvatake <demonvatake@gmail.com>
2024-07-23 11:46:40 +0300

    task1

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Ввод двух целых чисел
+        System.out.print("Введите первое число (a): ");
+        int a = scanner.nextInt();
+        System.out.print("Введите второе число (b): ");
+        int b = scanner.nextInt();
+
+        // Сравнение чисел
+        if (a > b) {
+            System.out.println("a > b");
+        } else if (a < b) {
+            System.out.println("a < b");
+        } else {
+            System.out.println("a = b");
+        }
+
+        // Выполнение арифметических операций
+        System.out.println("Сложение: a + b = " + (a + b));
+        System.out.println("Вычитание: a - b = " + (a - b));
+        System.out.println("Умножение: a * b = " + (a * b));
+
+        if (b != 0) {
+            System.out.println("Деление: a / b = " + ((double) a / b));
+        } else {
+            System.out.println("Деление на ноль невозможно");
+        }
+    }
+}
